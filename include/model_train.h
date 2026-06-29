f66e286c56b27db2fcd24be71da0dcc7b2cd9028
AalbatrossGuy <thexcelsiorisback@gmail.com>
2026-06-29 10:42:07 +0530

    Header file for model_train.c

---
+++ include/model_train.h
@@ -0,0 +1,27 @@
+#ifndef MODEL_TRAIN_H
+#define MODEL_TRAIN_H
+
+#include "computation_engine.h"
+
+typedef struct {
+  float *training_images;
+  float *training_labels;
+
+  float *test_images;
+  float *test_labels;
+
+  int training_samples;
+  int test_samples;
+  int in_dim;
+  int out_dim;
+  int epochs;
+  int batch_size;
+  float lr;
+} TrainingParams;
+
+void get_model_prediction(CompiledGraph *graph, const float *input_data);
+void train_model(CompiledGraph *graph, TrainingParams *config);
+void evaluate_model_prediction(CompiledGraph *graph, TrainingParams *config);
+void model_weight_matrix(GraphNode *weight_node);
+
+#endif // !MODEL_TRAIN_H
