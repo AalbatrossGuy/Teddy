33f781e3c7a97f7a8b5fa0b92f5f0643c573687c
AalbatrossGuy <thexcelsiorisback@gmail.com>
2026-06-29 17:35:11 +0530

    Add func for shuffling samples before each epoch

--- src/model_train.c
+++ src/model_train.c
@@ -12,3 +12,17 @@ void model_weight_matrix(GraphNode *weight_node) {
     float bound = sqrtf(6.0f / (float)(neuron_in_count + neuron_out_count));
     matrix_fill_random(weight_node->value, -bound, bound);
 }
+
+void get_model_prediction(ComputationGraph *graph, const float *input_data) {
+    matrix_upload(graph->input_node->value, input_data);
+    compiled_graph_forward(graph->graph_forward);
+}
+
+static void shuffle_samples(int *indices, int count) {
+    for (int i = count - 1; i > 0; i--) {
+        int j = rand() % (i + 1);
+        int temp = indices[i];
+        indices[i] = indices[j];
+        indices[j] = temp;
+    }
+}
